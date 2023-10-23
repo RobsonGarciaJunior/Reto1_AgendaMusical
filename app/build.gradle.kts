73bb344b5ce88109b97fa1d83a9fae3f61cbb8b0
in2dm3-v <ibai.gonzalezug@elorrieta-errekamari.com>
2023-10-23 20:58:24 +0200

    add-main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.agenda_musical_reto1"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.agenda_musical_reto1"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
