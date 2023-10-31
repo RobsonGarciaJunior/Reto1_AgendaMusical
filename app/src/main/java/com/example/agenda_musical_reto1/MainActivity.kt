b38a27050d064c8d2cc8abeaeacb519670e5f9b5
in2dm3-v <ibai.gonzalezug@elorrieta-errekamari.com>
2023-10-31 15:22:18 +0100

    add-intents

--- app/src/main/java/com/example/agenda_musical_reto1/MainActivity.kt
+++ app/src/main/java/com/example/agenda_musical_reto1/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.agenda_musical_reto1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.configuration_activity)
+        setContentView(R.layout.activity_main)
+
+
+        findViewById<TextView>(R.id.guest_link).setOnClickListener() {
+            val intent = Intent(this, MainMenuActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.register_button).setOnClickListener() {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.login_button).setOnClickListener() {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
+
