a7b57d2316e75703db97443e3b2a344dd1000d01
in2dm3-v <ibai.gonzalezug@elorrieta-errekamari.com>
2023-11-09 15:20:07 +0100

    [ADD] register validation

--- app/src/main/java/com/example/agenda_musical_reto1/RegisterActivity.kt
+++ app/src/main/java/com/example/agenda_musical_reto1/RegisterActivity.kt
@@ -1,12 +1,16 @@
 package com.example.agenda_musical_reto1
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
+import com.example.agenda_musical_reto1.utils.ValidationUtils
 
 class RegisterActivity : AppCompatActivity() {
+    @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_activity)
@@ -23,12 +27,53 @@ class RegisterActivity : AppCompatActivity() {
             finish()
         }
 
+        findViewById<Button>(R.id.register_button).setOnClickListener() {
+            val formEmail = findViewById<TextView>(R.id.mail_text).text.toString()
+            val formPassword = findViewById<TextView>(R.id.password_text_register).text.toString()
+            val formPasswordRepeat =
+                findViewById<TextView>(R.id.password_repeat_register_text).text.toString()
+            if (ValidationUtils.isEmailValid(formEmail)) {
+                if (ValidationUtils.arePasswordsMatching(formPassword, formPasswordRepeat)) {
+                    if (ValidationUtils.areAllFieldsFilled(
+                            formEmail,
+                            formPassword,
+                            formPasswordRepeat
+                        )
+                    ) {
+                        val intent = Intent(this, LoginActivity::class.java)
+                        startActivity(intent)
+                        finish()
+
+                    } else {
+                        findViewById<TextView>(R.id.password_repeat_register_text).error =
+                            "Rellena todos los campos"
+                    }
+                } else {
+                    findViewById<TextView>(R.id.password_repeat_register_text).error =
+                        "Las contraseñas no coinciden"
+                }
+
+            } else {
+                findViewById<TextView>(R.id.mail_text).error = "Formato del Email incorrecto"
+            }
+        }
+
         val spinnerButton = findViewById<ImageButton>(R.id.menuSpinner)
 
-        val optionActions = mapOf(
+        mapOf(
             "Inicio" to { MenuOptionsHandler.handleMenuOption("Inicio", this) },
-            "Todas las Canciones" to { MenuOptionsHandler.handleMenuOption("Todas las Canciones", this) },
-            "Mis Canciones Favoritas" to { MenuOptionsHandler.handleMenuOption("Mis Canciones Favoritas", this) }
+            "Todas las Canciones" to {
+                MenuOptionsHandler.handleMenuOption(
+                    "Todas las Canciones",
+                    this
+                )
+            },
+            "Mis Canciones Favoritas" to {
+                MenuOptionsHandler.handleMenuOption(
+                    "Mis Canciones Favoritas",
+                    this
+                )
+            }
         )
 
         Spinner.setupPopupMenu(spinnerButton, this)
